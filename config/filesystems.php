c7e9a85e324f145eb2a9bd763bbdce91cc7e2729
RyanAidilPratama <fajrianaidilp@gmail.com>
2020-11-11 09:37:47 +0800

    ref : refactor filesystem.php to make it work on shared hosting

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,8 +50,9 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            // 'root' => storage_path('app/public'),
+            'root' => public_path() . '/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
