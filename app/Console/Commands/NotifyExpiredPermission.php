5856d528bfeef6b68feda5fa1f7547883946ab7e
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-10 12:26:58 +0800

    chore : add check for expired permission and notify the user

---
+++ app/Console/Commands/NotifyExpiredPermission.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\AbsentPermission;
+use App\Notifications\AbsentPermissionExpiredNotification;
+use Illuminate\Console\Command;
+
+class NotifyExpiredPermission extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'permission:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check for expired absent permission';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $permissions = AbsentPermission::with(['user'])->whereDate('due_date', '=', today())->get();
+        foreach ($permissions as $permission) {
+            $permission->user->notify(new AbsentPermissionExpiredNotification($permission));
+        }
+        return 0;
+    }
+}
