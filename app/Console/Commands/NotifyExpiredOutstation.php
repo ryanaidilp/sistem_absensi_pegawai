efb1294f70e57d99e738835db0ce28aa65249ce2
ryanaidilp <fajrianaidilp@gmail.com>
2020-12-10 12:27:35 +0800

    chore : add command to check for expired outstations and notify its owner

---
+++ app/Console/Commands/NotifyExpiredOutstation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Outstation;
+use App\Notifications\OutstationExpiredNotification;
+use Illuminate\Console\Command;
+
+class NotifyExpiredOutstation extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'outstation:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check for expired outstation';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $outstations = Outstation::with(['user'])->whereDate('due_date', '=', today())->get();
+        foreach ($outstations as $outstation) {
+            $outstation->user->notify(new OutstationExpiredNotification($outstation));
+        }
+        return 0;
+    }
+}
