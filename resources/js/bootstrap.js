5b76e96813d9aaabe1282bedb4187394a0fc391f
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-19 18:46:18 +0800

    ref : remove bootstrap & popper

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,10 +7,10 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
+    // window.Popper = require('popper.js').default;
     // window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+    // require('bootstrap');
 } catch (e) {}
 
 /**
