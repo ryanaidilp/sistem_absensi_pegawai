5a947e97d30815e77f7925f3e63eca38bcc7b4b4
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-21 01:45:15 +0800

    make : create user repository interface

---
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface UserRepositoryInterface
+{
+    public function all();
+    public function getByPhone($phone);
+    public function allExcept($exceptId);
+}
