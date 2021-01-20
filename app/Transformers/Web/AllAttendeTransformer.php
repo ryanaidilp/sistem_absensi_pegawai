5c52c8c34ff68d66ce03e241b27f4768a4bd47dc
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-21 01:44:56 +0800

    chore : make all attende transformer

---
+++ app/Transformers/Web/AllAttendeTransformer.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Transformers\Web;
+
+
+
+use Carbon\Carbon;
+use App\Models\Attende;
+use League\Fractal\TransformerAbstract;
+
+class AllAttendeTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Attende $attende)
+    {
+        $status = $attende->status_kehadiran->name;
+        $date = Carbon::parse($attende->created_at);
+        if ($status === 'Terlambat') {
+            $status = $attende->status_kehadiran->name;
+            $status .= calculateLateTime($attende->kode_absen->start_time, $attende->attend_time, $date);
+        }
+        return [
+            'status' => $status,
+            'attend_time' => $attende->attend_time == null ? "-" : Carbon::parse($attende->attend_time)->format('H:i')
+        ];
+        return [
+            //
+        ];
+    }
+}
