24ed132978eff52e57183f6d8f70788d383e5b73
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-21 01:43:32 +0800

    chore : make all attende transformer

---
+++ app/Transformers/Web/AttendeUserTransformer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Transformers\Web;
+
+use App\Models\User;
+use League\Fractal\TransformerAbstract;
+
+class AttendeUserTransformer extends TransformerAbstract
+{
+    private $presence;
+
+    public function __construct($presence)
+    {
+        $this->presence = $presence;
+    }
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'name' => $user->name,
+            'status' => $user->status,
+            'nip' => $user->nip,
+            'department' => $user->departemen->name,
+            'position' => $user->position,
+            'presensi' => $this->presence
+        ];
+    }
+}
