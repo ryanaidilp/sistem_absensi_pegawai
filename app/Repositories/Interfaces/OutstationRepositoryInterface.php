5fcd0ad213ee1362b789faacc3843993cfd6870f
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-21 01:46:29 +0800

    chore : make outstation repository interface

---
+++ app/Repositories/Interfaces/OutstationRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use Illuminate\Http\Request;
+
+interface OutstationRepositoryInterface
+{
+    public function all();
+    public function save(Request $request, $name = null, $id = null);
+    public function approve(Request $request);
+    public function getByUser($userId);
+    public function getBetweenDate($date);
+    public function getByUserAndYear($userId, $year);
+    public function getByUserAndStartDate($userId, $startDate);
+}
