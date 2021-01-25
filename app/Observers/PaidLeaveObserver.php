113ebd47876ead52e1efa8fe2fe51323529c6ad1
ryanaidilp <fajrianaidilp@gmail.com>
2021-01-25 17:46:44 +0800

    ref : extract reusable function from observer to helper

--- app/Observers/PaidLeaveObserver.php
+++ app/Observers/PaidLeaveObserver.php
@@ -3,10 +3,9 @@
 namespace App\Observers;
 
 use App\Models\Attende;
+use App\Models\PaidLeave;
 use App\Models\AttendeStatus;
 use App\Models\LeaveCategory;
-use App\Models\PaidLeave;
-use Carbon\Carbon;
 
 class PaidLeaveObserver
 {
@@ -18,9 +17,7 @@ class PaidLeaveObserver
      */
     public function created(PaidLeave $paidLeave)
     {
-        $category = LeaveCategory::where('id', $paidLeave->leave_category_id)->first();
-        $status = AttendeStatus::where('name', $category->name)->first();
-        $this->updateStatus(Attende::ABSENT, $status->id, $paidLeave);
+        // 
     }
 
     /**
@@ -34,32 +31,9 @@ class PaidLeaveObserver
         $category = LeaveCategory::where('id', $paidLeave->leave_category_id)->first();
         $status = AttendeStatus::where('name', $category->name)->first();
         if ($paidLeave->is_approved) {
-            $this->updateStatus(Attende::ABSENT, $status->id, $paidLeave);
+            updateStatus(Attende::ABSENT, $status->id, $paidLeave);
         } else {
-            $this->updateStatus($status->id, Attende::ABSENT, $paidLeave);
-        }
-    }
-
-    private function updateStatus($from, $to, PaidLeave $paidLeave)
-    {
-
-        if (
-            Carbon::parse($paidLeave->due_date)->isBefore(today()) ||
-            Carbon::parse($paidLeave->start_date)->isBefore(today())
-        ) {
-            $presences = $paidLeave->user->presensi()
-                ->whereDate('created_at', '>=', Carbon::parse($paidLeave->start_date)->toDateString())
-                ->whereDate('created_at', '<=',  Carbon::parse($paidLeave->due_date)->toDateString())
-                ->where('attende_status_id', $from)->get();
-        } else if (Carbon::parse($paidLeave->start_date)->isToday()) {
-            $presences = $paidLeave->user->presensi()->today()->where('attende_status_id', $from)->get();
-        } else {
-            return;
-        }
-        foreach ($presences as $presence) {
-            $presence->update([
-                'attende_status_id' => $to
-            ]);
+            updateStatus($status->id, Attende::ABSENT, $paidLeave);
         }
     }
 
@@ -74,7 +48,7 @@ class PaidLeaveObserver
         $category = LeaveCategory::where('id', $paidLeave->leave_category_id)->first();
         $status = AttendeStatus::where('name', $category->name)->first();
         if ($paidLeave->is_approved) {
-            $this->updateStatus($status->id, Attende::ABSENT, $paidLeave);
+            updateStatus($status->id, Attende::ABSENT, $paidLeave);
         }
     }
 
