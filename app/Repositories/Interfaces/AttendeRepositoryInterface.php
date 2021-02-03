a542d7ae98915134b05ef24b575e190a2471e185
ryanaidilp <fajrianaidilp@gmail.com>
2021-02-03 18:55:36 +0800

    chore : add function to get attende data by month

--- app/Repositories/Interfaces/AttendeRepositoryInterface.php
+++ app/Repositories/Interfaces/AttendeRepositoryInterface.php
@@ -10,6 +10,7 @@ interface AttendeRepositoryInterface
     public function cancel($attendanceId, $reason);
     public function presence(Request $request, $code, $attende);
     public function getByDate($date, $exludeUser = null);
+    public function getByMonth($date);
     public function getByUserAndCode($userId, $codeId);
     public function getByUserAndYear($userId, $year);
     public function formatUserAttendes($attendes);
