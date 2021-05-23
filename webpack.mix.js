e841e00d79526b1b3ccf4e2038737b0aef540410
ryanaidilp <fajrianaidilp@gmail.com>
2021-05-23 22:18:43 +0800

    ref : add .vue() in webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const tailwindcss = require('tailwindcss');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
